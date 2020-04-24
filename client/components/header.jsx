11038fc996c01918582eb77edc3ca6f8151b39f4
Logan Mulvihill <mulvihill.logan@gmail.com>
2020-04-23 21:56:32 -0700

    added productlistitem component

---
+++ client/components/header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Header(props) {
+
+  return (
+    <div>
+      <i className="fas fa-dollar-sign"></i>
+      Wicked Sales
+    </div>
+  );
+
+}
+
+export default Header;
---
+++ client/components/productlistitem.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Product() {
+  return (
+    <div className="card">
+      <div className="card-body">
+        <h5 className="card-title">Product Name</h5>
+        <p className="card-text">$0.00</p>
+        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+      </div>
+    </div>
+  );
+}
+
+export default Product;
