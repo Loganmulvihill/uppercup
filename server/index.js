dc3fc729511d2ed1468b1ce6b40267324399d9b4
Logan Mulvihill <mulvihill.logan@gmail.com>
2020-04-24 21:24:39 -0700

    completed feature 3

--- server/index.js
+++ server/index.js
@@ -37,6 +37,32 @@ app.get('/api/products', (req, res, next) => {
     .catch(err => next(err));
 });
 
+app.get('/api/products/:productId', (req, res, next) => {
+
+  const { productId } = req.params;
+  const sql = `
+     select *
+      from "products"
+    where "productId" = $1
+  `;
+
+  const params = [productId];
+
+  db.query(sql, params)
+    .then(result => {
+      const product = result.rows[0];
+      if (!product) {
+        res.status(404).json({
+          error: `Cannot find product with productId ${productId}`
+        });
+      } else {
+        res.json(product);
+      }
+    })
+    .catch(err => next(err));
+
+});
+
 app.use('/api', (req, res, next) => {
   next(new ClientError(`cannot ${req.method} ${req.originalUrl}`, 404));
 });
