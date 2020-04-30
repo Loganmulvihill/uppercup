307f93698005cfdb1e7f1ef64d775cf6fea706c1
Logan Mulvihill <mulvihill.logan@gmail.com>
2020-04-29 21:22:11 -0700

    fixing first promise then

--- server/index.js
+++ server/index.js
@@ -70,6 +70,85 @@ app.get('/api/products/:productId', (req, res, next) => {
 
 });
 
+app.get('/api/cart', (req, res, next) => {
+  db.query('select \'successfully connected\' as "message"')
+    .then(result => res.json([]))
+    .catch(err => next(err));
+});
+
+// app.get('/api/cart', (req, res, next) => {
+
+//   const sql = `
+//     select *
+//       from "carts"
+//   `;
+//   db.query(sql)
+//     .then(result => {
+//       res.json(result.rows[0]);
+//     })
+//     .catch(err => next(err));
+// });
+
+app.post('/api/cart', (req, res, next) => {
+  const product = req.body;
+  const productId = req.body.productId;
+  if (!parseInt(product.productId) < 0) {
+    return res.status(400).json({
+      error: 'product id must be positive'
+    });
+  }
+
+  const sql = `
+  select "price"
+     where "productId" = $1
+     from "products"
+    returning *`;
+
+  const params = [productId];
+  db.query(sql, params)
+    .then(result => {
+      const product = result.rows[0];
+      if (!product) {
+        throw new ClientError('This does not work', 400);
+      }
+      const sql = ` insert into "carts"("cartId", "createdAt")
+      values(default, default )
+      returning "cartId"`;
+
+    });
+  // .then(result => {
+  //   const grade = result.rows[0];
+  //   res.json(grade);
+  // })
+// if (!newGrade.course) {
+//   return res.status(400).json({
+//     error: 'a course is required'
+//   });
+// }
+// if (!newGrade.name) {
+//   return res.status(400).json({
+//     error: 'a name is required'
+//   });
+// }
+// const sql =
+//   insert into "grades" ("name", "course", "grade")
+//   values ($1, $2, $3)
+//     returning *
+// `;
+// const params = [newGrade.name, newGrade.course, parseInt(newGrade.grade)];
+// db.query(sql, params)
+//   .then(result => {
+//     const grade = result.rows[0];
+//     res.json(grade);
+//   })
+//   .catch(err => {
+//     console.error(err);
+//     res.status(500).json({
+//       error: 'An unexpected error occurred.'
+//     });
+//   });
+});
+
 app.use('/api', (req, res, next) => {
   next(new ClientError(`cannot ${req.method} ${req.originalUrl}`, 404));
 });
