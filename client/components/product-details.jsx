c5ad59a4e5e748009715197e3f2be3d3534d39ab
Logan Mulvihill <mulvihill.logan@gmail.com>
2020-06-09 15:40:12 -0500

    mobile and desktop resonsiveness acquired

--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -19,25 +19,21 @@ export default class Productdetails extends React.Component {
   render() {
     if (this.state.product !== null) {
       return (
-        <div className="container mt-5 border rounded">
-          <button onClick={() => this.props.setView('catalog', {})} className="row mb-2 text-muted btn bg-transparent">
+        <div className="container mt-5 mb-5 border rounded white-background shadow-sm">
+          <button onClick={() => this.props.setView('catalog', {})} className=" text-muted btn bg-transparent pl-2 pr-2 pt-2">
             {'<'} Back to Catalog button
           </button>
-          <div className="row mb-2">
-            <div className="col-5">
-              <img src={this.state.product.image} className="card-img-top product-details-img" alt={this.state.product.name} />
-            </div>
-            <div className="col-6">
-              <div className="card-body">
-                <h3 className="card-title">{this.state.product.name}</h3>
-                <p className="card-text text-muted">{`$ ${(this.state.product.price.toFixed(2)) / (100)}`}</p>
-                <p className="card-text">{this.state.product.shortDescription}</p>
-                <button type="button" onClick={() => this.props.addToCart(this.state.product)} className="btn btn-primary">Add to Cart</button>
-              </div>
+          <div className="row p-2 no-gutters">
+            <img src={this.state.product.image} className="card-img-top product-details-img col-sm-5" alt={this.state.product.name} />
+            <div className="col-sm-6 d-flex justify-content-center flex-column align-items-center pl-1">
+              <h2 className="card-title">{this.state.product.name}</h2>
+              <h3 className="card-text text-primary">{`$ ${(this.state.product.price.toFixed(2)) / (100)}`}</h3>
+              <p className="card-text text-center">{this.state.product.shortDescription}</p>
+              <button type="button" onClick={() => this.props.addToCart(this.state.product)} className="btn btn-primary">Add to Cart</button>
             </div>
           </div>
-          <div className="row">
-            <p className="card-text">{this.state.product.longDescription}</p>
+          <div className="col pl-2 pr-2 pb-4 pt-2">
+            <p className="card-text text-cetner">{this.state.product.longDescription}</p>
           </div>
         </div>
       );
