bc5106e2c7490b0b252a52db236c297e4be1f786
Logan Mulvihill <mulvihill.logan@gmail.com>
2020-06-30 16:57:08 -0500

    added margin to cup and cart icon to provide space between text in header

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -6,12 +6,12 @@ function Header(props) {
     <div className="header-container d-flex justify-content-between">
       <div className="container d-flex justify-content-between">
         <div>
-          <i className="fas fa-coffee header-icon"></i>
+          <i className="fas fa-coffee header-icon mr-1"></i>
       UpperCup
         </div>
         <div className="cursor-pointer" onClick={() => props.setView('cart', {})}>
           {`${cartItemCount} Items`}
-          <i className="fas fa-shopping-cart"></i>
+          <i className="fas fa-shopping-cart ml-1"></i>
         </div>
       </div>
     </div>
