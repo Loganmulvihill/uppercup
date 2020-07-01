48a4b18f0a062a40ac6aed7c53b4768ca98f598a
Logan Mulvihill <mulvihill.logan@gmail.com>
2020-06-30 21:54:34 -0500

    added w-100 class to card within cart-summary-item component and deleted redundant media queries

--- client/components/cart-summary-item.jsx
+++ client/components/cart-summary-item.jsx
@@ -4,7 +4,7 @@ function CartSummaryItem(props) {
   const cartItem = props.cartItem;
 
   return (
-    <div className="card mb-5" >
+    <div className="card mb-5 w-100" >
       <div key={cartItem.cartItemId} className="row no-gutters p-2 pt-3 d-flex justify-content-around shadow-sm align-items-center">
         <img className="col-sm-5 mr-2" src={cartItem.image} alt={cartItem.name} />
         <div className="col-sm-6">
